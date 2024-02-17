db182e2a283964f605434729a66d94c422a08e73
cdgraybill <cdgraybill@gmail.com>
2024-02-17 12:27:26 -0600

    the absolute bare minimum

--- docs/src/App.tsx
+++ docs/src/App.tsx
@@ -5,9 +5,6 @@ function App() {
   return (
     <>
       <LinkSection />
-      <div>
-        main site content
-      </div>
     </>
   );
 }
--- docs/src/components/LinkSection.tsx
+++ docs/src/components/LinkSection.tsx
@@ -1,10 +1,11 @@
-
 function LinkSection() {
 
     return (
         <div>
-            <a href="/blog">Blog</a>
-            <a href="/coolstuff">Cool Stuff I've Found</a>
+            <h1>Hey there, I'm Cam</h1>
+            <h2>I'm a software engineer</h2>
+            <h4><a href="https://github.com/cdgraybill">Here's some code I've written</a></h4>
+            <h4><a href="https://www.linkedin.com/in/camerongraybill/">And here's where you can get in touch with me</a></h4>
         </div>
     )
 }
