587d19e0088cd5bfcb7f12ad8bd468b24f0364e5
cdgraybill <cdgraybill@gmail.com>
2024-06-24 11:24:25 -0500

    playing with tailwind

--- docs/src/App.tsx
+++ docs/src/App.tsx
@@ -1,18 +1,15 @@
 import { Link, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home/Home";
-import Music from "./components/Music/Music";
 
 function App() {
   return (
     <>
       <nav>
-        <Link to="/"> Home</Link> | 
-        <Link to="/music"> Music</Link>
+        <Link to="/"> Home</Link>
       </nav>
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        <Route path="/music" element={<Music />}></Route>
       </Routes>
     </>
   );
--- docs/src/components/Home/Home.tsx
+++ docs/src/components/Home/Home.tsx
@@ -5,19 +5,19 @@ function Home() {
 
     return (
       <div>
-        <h1>Hey there, I'm Cam</h1>
-        <h2>I'm a software engineer</h2>
+        <h1 className="leading-normal text-5xl font-bold">Hey there, I'm Cam</h1>
+        <h2 className="leading-normal text-2xl font-bold">I'm a software engineer</h2>
         <h4>
-          <Link to={"https://github.com/cdgraybill"}>
+          <Link className="leading-loose" to={"https://github.com/cdgraybill"}>
             Here's some code I've written
           </Link>
         </h4>
         <h4>
-          <Link to="https://www.linkedin.com/in/camerongraybill/">
+          <Link className="leading-loose" to="https://www.linkedin.com/in/camerongraybill/">
             Here's where you can get in touch with me
           </Link>
         </h4>
-        <h4>
+        <h4 className="leading-loose">
             And for everything else, check the links above
         </h4>
       </div>
