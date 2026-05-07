682a3cd96d7cf9b89f07d90b38da081845d0384d
cdgraybill <cdgraybill@gmail.com>
2026-05-06 23:22:59 -0500

    bugs

--- docs/script.js
+++ docs/script.js
@@ -9,3 +9,19 @@ function showSection(id, event) {
   document.getElementById(id).classList.add("active");
   document.getElementById("nav-" + id).classList.add("active");
 }
+
+function changeSkin(id) {
+  switch (id) {
+    case "ares":
+      document.documentElement.style.setProperty("--nav-selected", "#b83228");
+      document.documentElement.style.setProperty("--bg", "#0d0d0b");
+      document.documentElement.style.setProperty("--text-primary", "#ddddd5");
+      break;
+    case "volt":
+      document.documentElement.style.setProperty("--nav-selected", "#00E320");
+      document.documentElement.style.setProperty("--bg", "#9E9E9E");
+      document.documentElement.style.setProperty("--text-primary", "#0d0d0b");
+      break;
+    default:
+  }
+}
